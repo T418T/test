f07aa752af1951150ef6093a10157d94a18b7759
T418T <ouc2015462@gmail.com>
2024-12-09 16:02:22 +0900

    Delete xss.js

--- xss.js
+++
@@ -1,9 +0,0 @@
-function displayMessage() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const message = urlParams.get('message');
-    if (message) {
-        // 悪意のあるスクリプトがそのまま挿入される
-        document.getElementById('output').innerHTML = message;
-    }
-}
-window.onload = displayMessage;
